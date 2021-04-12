f609608a187b6fad2b0e262a54bb3b53607c98c5
Ghost4dot2 <ngroggett@gmail.com>
2021-04-12 02:53:42 -0700

    General updates to hexagon to allow directional halves, update to grid allowing a square off functionality still a work in progress

--- Grid.gd
+++ Grid.gd
@@ -3,53 +3,104 @@ extends Node2D
 
 var Hex = preload("res://Hexagon.tscn")
 
-export(int, 1, 9000) var hexSize = 50
-export(int, 1, 100) var gridHeight = 1 
-export(int, 1, 100) var gridWidth = 1
-export(bool) var squareOff = false
+export(int, 1, 9000) var hexSize = 50 setget set_grid_size
+export(int, 1, 100) var gridHeight = 1 setget set_grid_height
+export(int, 1, 100) var gridWidth = 1 setget set_grid_width
+export(bool) var squareOff = false setget set_squareoff
 
 var currentGridHeight : int = 1 
 var currentGridWidth : int = 1
 
 var hexagons : Array
 
+func set_grid_size(input):
+	hexSize = input
+	for hex in hexagons:
+		hex.hexSize = hexSize
+		hex.updatePosition()
+
+func set_grid_height(input):
+	gridHeight = input
+	clearGrid()
+	generateGrid()
+
+func set_grid_width(input):
+	gridWidth = input
+	clearGrid()
+	generateGrid()
+
+func set_squareoff(input):
+	squareOff = input
+	clearGrid()
+	generateGrid()
+
 func clearGrid():
 	if not hexagons.empty():
 		hexagons.clear()
-	for child in $"collection".get_children():
-		$"collection".remove_child(child)
+	for child in $".".get_children():
+		$".".remove_child(child)
 
 func generateGrid():
-	#creates initial hexs
+	if squareOff:
+		generateSquareGrid()
+	else:
+		generateRegularGrid()
+
+func generateRegularGrid():
+	#creates initial hexes
 	var numberOfHexs = gridHeight * gridWidth
-	print(numberOfHexs)
+	createHexes(numberOfHexs)
+	calculateCoordinates()
+
+func generateSquareGrid():
+	var numberOfHexs = gridHeight * gridWidth + gridWidth/2 + gridWidth % 2
+	createHexes(numberOfHexs)
+	squareOff()
+	calculateCoordinates()
+	squareOffCoordinates()
+
+func createHexes(numberOfHexs : int):
 	for _i in range(0, numberOfHexs):
 		var newHex = Hex.instance()
+		newHex.hexType = "Full"
 		newHex.hexSize = hexSize
 		hexagons.append(newHex)
-		$"collection".add_child(newHex)
-	
+		$".".add_child(newHex)
+
+func calculateCoordinates():
 	#sets grid coordinates
 	for row in range(0, gridHeight):
 		for col in range(0, gridWidth):
 			var pos = col + row*(gridWidth)
 			hexagons[pos].offsetToCube(col, row)
 			hexagons[pos].updatePosition()
-			hexagons[pos].debug()
-			#print(col, " ", row, " : ", hexagons[pos].q(), " ", hexagons[pos].r(), " ", hexagons[pos].s(), ": ", hexagons[pos].total() )
-	"""
-	#sets actual position of grid
-	for hex in hexagons:
-		hex.updatePosition()
-		#hex.debug()
-	"""
+			#hexagons[pos].debug()
 
-func _process(_delta):
-	if currentGridHeight != gridHeight or currentGridWidth != gridWidth:
-		clearGrid()
-		generateGrid()
-		currentGridHeight = gridHeight
-		currentGridWidth = gridWidth
+func squareOffCoordinates():
+	for _i in range(0, gridWidth/2 + gridWidth % 2):
+		var pos = gridHeight * gridWidth + _i
+		hexagons[pos].offsetToCube(_i * 2, gridWidth-2)
+		hexagons[pos].updatePosition()
+		hexagons[pos].hexType = "Half Top"
+		print(pos)
+
+func squareOff():
+	var col = 0
+	while col < gridWidth:
+		hexagons[col].hexType = "Half Bottom"
+		col = col + 2
+
+
+"""
+func calculateHalfHex() -> int:
+	var halfHexes = 0
+	if squareOff:
+		if gridWidth % 2 == 1:
+			halfHexes = gridWidth - 1
+		else:
+			halfHexes = gridWidth
+	return halfHexes 
+"""
 
 
 # Called when the node enters the scene tree for the first time.
--- Hexagon.gd
+++ Hexagon.gd
@@ -1,17 +1,35 @@
 tool
 extends Node2D
 
-export(int, 1, 9000) var hexSize = 40
+export(int, 1, 9000) var hexSize = 40 setget on_hexsize_change
+export (String, "Full", "Half Top", "Half Bottom", "Half Left", "Half Right") var hexType : String = "Full" setget on_hextype_change
 var prevSize: int = 0
 
-var hexHalf : bool = false
 var disabled : bool = false
 
 var _q : int = 0
 var _r : int = 0
 var _s : int = 0
 
+func on_hextype_change(input):
+	hexType = input
+	setVertices(vertices(hexSize))
+
+func on_hexsize_change(input):
+	hexSize = input
+	setVertices(vertices(hexSize))
+
 func vertices( _size : int, center : Vector2 = Vector2(0,0) ) -> Array:
+	#print(hexType)
+	if hexType == "Half Top" or hexType == "Half Bottom":
+		return tb_halfVertices(_size, center)
+	elif hexType == "Half Left" or hexType == "Half Right":
+		return lr_halfVertices(_size, center)
+	else:
+		return normalVertices(_size, center)
+
+
+func normalVertices(_size : int, center : Vector2 = Vector2(0,0) ) -> Array:
 	var points : Array =[]
 	for i in range(0,6):
 		var angle_deg = 60 * i 
@@ -19,6 +37,44 @@ func vertices( _size : int, center : Vector2 = Vector2(0,0) ) -> Array:
 		points.append(Vector2(center.x + _size * cos(angle_rad), \
 							  center.y + _size * sin(angle_rad)))
 	
+	return points
+
+func tb_halfVertices(_size : int, center : Vector2 = Vector2(0,0)) -> Array:
+	var points : Array = []
+	for i in range(0,4):
+		var angle_deg = 0
+		if hexType == "Half Bottom":
+			angle_deg = 60 * i
+		else:
+			angle_deg = 60 * -i
+
+		var angle_rad = PI / 180 * angle_deg
+		points.append(Vector2(center.x + _size * cos(angle_rad), \
+							  center.y + _size * sin(angle_rad)))
+	#Append first point again
+	points.append(Vector2(points[0].x, points[0].y))
+	
+	return points
+
+func lr_halfVertices(_size : int, center : Vector2 = Vector2(0,0)) -> Array:
+	var points : Array = []
+	points.append(Vector2(center.x + _size * cos( PI / 180 * 90), \
+						  center.y + _size * sin( PI / 180 * 60)))
+	for i in range(0,3):
+		var angle_deg = 0
+		if hexType == "Half Right":
+			angle_deg = 60 - i * 60
+		else:
+			angle_deg = -240 + i * 60
+		var angle_rad = PI / 180 * angle_deg
+		points.append(Vector2(center.x + _size * cos(angle_rad), \
+							  center.y + _size * sin(angle_rad)))
+	
+	points.append(Vector2(center.x + _size * cos( PI / 180 * -90), \
+						  center.y + _size * sin( PI / 180 * -60)))
+	
+	#Append first point again
+	points.append(Vector2(points[0].x, points[0].y))
 	return points
 
 func setCoordinate(q : int, r : int):
@@ -39,11 +95,13 @@ func setVertices(vertices : Array):
 	#add first point as last
 
 	$"Line2D".add_point(vertices[0])
+	$"Area2D/CollisionPolygon2D".polygon = $"Line2D".points
 	#print($"Line2D".points)
 
 func calculatePixelPosition() -> Vector2:
-	return Vector2(float(hexSize) * 3.0/2.0 * float(_q), \
-				   float(hexSize) * (sqrt(3.0)/2.0 * _q + sqrt(3.0) * float(_r)))
+	# the .5 is for the line width
+	return Vector2(float(hexSize + .5) * 3.0/2.0 * float(_q), \
+				   float(hexSize + .5) * (sqrt(3.0)/2.0 * _q + sqrt(3.0) * float(_r)))
 
 func updatePosition():
 	set_position(calculatePixelPosition())
@@ -74,9 +132,10 @@ func _ready():
 	pass # Replace with function body.
 
 func _process(_delta):
-	if prevSize != hexSize:
-		prevSize = hexSize
-		setVertices(vertices(hexSize))
+#	if prevSize != hexSize:
+#		prevSize = hexSize
+#		setVertices(vertices(hexSize))
+	pass
 
 func _on_Area2D_mouse_entered():
 	print("Found Polygon " + String(_q) + ", " + String(_r))
